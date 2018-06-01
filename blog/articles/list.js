470b8f6bcd73b0c658bed4d472e5c1d6f5ba55fb
soroosh sarabadani <soroosh.sarabadani@klarna.com>
2018-06-01 14:21:42 +0200

    start fandogh introduction

--- blog/articles/list.js
+++ blog/articles/list.js
@@ -1,11 +1,18 @@
 module.exports = function () {
-  return [
-    {
-      title: 'روش نوشتن یک Dockerfile',
-      description: 'یکی از بهترین روشهای تولید یک تصویر داکر (docker image)، نوشتن یک Dockerfile و سپس استفاده از دستور docker build است. این روش به دلیل سادگی و سازگاری با متدولوژی زیرساخت به صورت کد (Infrastructure as Code) به متداولترین راه برای تولید تصاویر داکر تبدیل شده است.',
-      thumbnail: '/articles/dockerfile.png',
-      link: '/articles/how-write-docker-file.html',
-      date: '۳۰ اردیبهشت ۱۳۹۷',
-    }
-  ]
+    return [
+        {
+            title: 'معرفی فندق',
+            description: 'در واقع فندق سکویی است که سعی در حل کردن چالشهای مختلفی دارد که در زمان استفرار محصول تیمهای توسعه و نگهداری نرم افزار با آنها برخورد میکنند.',
+            thumbnail: '/articles/dockerfile.png',
+            link: '/articles/fandogh-introduction.html',
+            date: '۲ خرداد ۱۳۹۷'
+        },
+        {
+            title: 'روش نوشتن یک Dockerfile',
+            description: 'یکی از بهترین روشهای تولید یک تصویر داکر (docker image)، نوشتن یک Dockerfile و سپس استفاده از دستور docker build است. این روش به دلیل سادگی و سازگاری با متدولوژی زیرساخت به صورت کد (Infrastructure as Code) به متداولترین راه برای تولید تصاویر داکر تبدیل شده است.',
+            thumbnail: '/articles/dockerfile.png',
+            link: '/articles/how-write-docker-file.html',
+            date: '۳۰ اردیبهشت ۱۳۹۷',
+        }
+    ]
 }
