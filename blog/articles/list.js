9a1daf90c78c91c30f9e027b6d1dffcd903f0734
soroosh sarabadani <soroosh.sarabadani@gmail.com>
2018-06-03 13:18:21 +0200

    change introduction thumbnail

--- blog/articles/list.js
+++ blog/articles/list.js
@@ -3,7 +3,7 @@ module.exports = function () {
         {
             title: 'معرفی فندق',
             description: 'در واقع فندق سکویی است که سعی در حل کردن چالشهای مختلفی دارد که در زمان استفرار محصول تیمهای توسعه و نگهداری نرم افزار با آنها برخورد میکنند.',
-            thumbnail: '/articles/dockerfile.png',
+            thumbnail: '/articles/fandogh.svg',
             link: '/articles/fandogh-introduction.html',
             date: '۲ خرداد ۱۳۹۷'
         },
